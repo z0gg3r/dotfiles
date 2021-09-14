f2a666d4a84697e90d6bf9e481d9d7710e9377d0
z0gg3r <z0gg3r@users.no-reply.github.com>
2021-09-15 01:39:05 +0200

    New script!

---
+++ scripts/remove_useless_files.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+remove_file()
+{
+	if [ -e "$1" ]
+	then
+		rm "$1"
+	fi
+}
+
+remove_file $HOME/.lesshst
+remove_file $HOME/.calc_history
+remove_file $HOME/.wget-hsts
+remove_file $HOME/.xsession-errors.old
+remove_file $HOME/.sqlite_history
