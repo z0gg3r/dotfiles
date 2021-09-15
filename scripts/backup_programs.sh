8d674ffa34e643206df6f506dc55caea64b78565
z0gg3r <z0gg3r@users.no-reply.github.com>
2021-09-15 13:04:00 +0200

    Update and deploy function

--- scripts/backup_programs.sh
+++ scripts/backup_programs.sh
@@ -7,21 +7,49 @@ die()
 	echo "$1"
 	exit
 }
-echo "Prog. Dir.:$PROG_DIR"
-echo "Backup: $BAK"
-cwd=$(pwd)
-cd "$PROG_DIR" || die "Cannot enter $PROG_DIR."
 
-for dir in *
-do
-	echo "Entering $dir"
-	cd "$dir" || die "Cannot enter $dir"
-	if [ -e ".git/" ]
-	then
-		remote="$(git remote get-url origin)"
-		echo "$remote" >> "$BAK"
-	fi
-	cd ..
-done
+update()
+{
+	rm "$BAK"
+	cwd=$(pwd)
+	cd "$PROG_DIR" || die "Cannot enter $PROG_DIR"
+	for dir in *
+	do
+		echo "Entering $dir"
+		cd "$dir" || die "Cannot enter $dir"
+		if [ -e ".git/" ]
+		then
+			remote="$(git remote get-url origin)"
+			echo "${remote%/} $dir" >> "$BAK"
+		fi
+		cd ..
+	done
+	cd "$cwd" || die "Cannot enter $cwd"
+}
 
-cd "$cwd" || die "Cannot enter $cwd"
+deploy()
+{
+	cwd=$(pwd)
+	uris=$(cat "$BAK")
+	cd "$PROG_DIR" || die "Cannot enter $PROG_DIR"
+	for uri in $uris
+	do
+		dir=$(cut -d " " -f2 "$uri")
+		if ! [ -e "$dir" ]
+		then
+			git clone "$uri"
+		fi
+	done
+}
+
+if [ -z "$1" ]
+then
+	update
+fi
+
+case $1 in
+	"update") update ;;
+	"up" ) update ;;
+	"deploy" ) deploy ;;
+	"dep" ) deploy ;;
+esac
