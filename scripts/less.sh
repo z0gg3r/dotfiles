90ddb7627074e13733311e8bfee6b067ae14ca3f
zocker <zocker@10zen.eu>
2021-09-21 09:39:07 +0200

    Fixes

--- scripts/less.sh
+++ scripts/less.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 filename="$1"
-num_spaces=$(((`tput cols`-`awk length > m { m = length } END { print m }` $filename`)/2))
+num_spaces=$(((`tput cols`-`awk 'length > m { m = length } END { print m }' "$filename"`)/2))
 spaces=""
 if [ $num_spaces -gt 0 ]
 then
-	spaces=`printf "%*s" $num_spaces
+	spaces=$(printf "%*s" $num_spaces " ")
 fi
-sed "s/^/$spaces/" $filename | less
+sed "s/^/$spaces/" "$filename" | less
