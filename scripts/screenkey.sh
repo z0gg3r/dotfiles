fe89fdd9ef686dc5beb383904cfde86bc8d14e3d
zocker <zocker@10zen.eu>
2021-09-25 11:31:25 +0200

    Script to toggle screenkey

---
+++ scripts/screenkey.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+if ! pgrep -u "$USER" "screenkey" > /dev/null
+then
+	notify-send "Starting screenkey..."
+	daemonize "$(which screenkey)"
+else
+	notify-send "Killing screenkey..."
+	killall screenkey
+fi
