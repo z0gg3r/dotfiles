49b4dc68182f0f2c90edeb7882a132e65d376396
zocker <zocker@10zen.eu>
2021-12-20 19:36:18 +0100

    Align log

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -7,4 +7,4 @@ wallpaper="$dir/$1.png"
 ln -sf "$wallpaper" /home/zocki/.wallpaper.png
 num="$(echo "$1 + 1" | calc -p)"
 name="$(lecho -c -f /home/zocki/.wallpapermap -l "$num" | trim)"
-echo "[Manual Change] $date @ $time -> $wallpaper ($name)" >> /home/zocki/.config/wallpaperlog
+echo "[Manual Change]    $date @ $time -> $wallpaper ($name)" >> /home/zocki/.config/wallpaperlog
