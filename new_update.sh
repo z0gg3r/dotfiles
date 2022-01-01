39eac57359091a7fb31bb0742f50e26e9f857de9
zocker <zocker@10zen.eu>
2022-01-01 12:51:00 +0100

    Changes to the new update script
    So there is some progress here. I will test this some more and hopefully
    this is mature enough (or is soon mature enough) to replace the old
    update script.

--- new_update.sh
+++ new_update.sh
@@ -2,57 +2,113 @@
 UPTODATE="\033[32;48m"
 CHANGE="\033[33;38m"
 ERROR="\033[31;48m"
-INFO="\033[30;48m"
+INFO="\033[35;48m"
 IGNORE="\033[34;48m"
 END="\033[0m"
 
-echo "$INFO Using $HOME as \$HOME $END"
+echo "$INFO Using $HOME as \$HOME ! $END"
+echo "$INFO Assuming echo does not need -e for colour output! $END"
+
+die()
+{
+	echo "$ERROR $1 $END"
+	exit 1
+}
 
 _diff()
 {
 	diff "$1" "$2" > /dev/null
 }
 
+# Args
+# $1 = Path to dir
+# $2 = Files to ignore
+update_dir()
+{
+	dir_path=$1
+	count=$(echo "$dir_path" | sed -e 's/\(.\)/\1\n/g' | grep -c "/")
+	count=$(echo "$count 1 +pq" | dc)
+	dir=$(echo "$dir_path" | cut -d/ -f$count)
+	ignore_list=$2
+	if ! [ -e "$dir_path" ]
+	then
+		die "$dir_path does not exist!"
+	fi
+	if ! [ -e "$dir" ]
+	then
+		mkdir "$dir"
+	fi
+	for file in $($(which ls) "$dir_path")
+	do
+		case $ignore_list in
+			*$file*) ignore="yes" ;;
+		esac
+		if [ -z "$ignore" ]
+		then
+			update_file "$dir_path/$file" "$dir/$file"
+		else
+			echo "$IGNORE $file will be ignored!$END"
+		fi
+		ignore=""
+	done
+}
+
+# Args 
+# $1 = Path to file
+# $2 = Path in repo
+update_file()
+{
+	if [ -z "$1" ]; then die "You have to give at least one argument"; fi
+	file_path="$1"
+	file_name="$2"
+	if [ -z "$file_name" ]
+	then
+		count=$(echo "$file_path" | sed -e 's/\(.\)/\1\n/g' | grep -c "/")
+		count=$(echo "$count 1 +pq" | dc)
+		file_name=$(echo "$file_path" | cut -d/ -f$count)
+	fi
+
+	count=$(echo "$file_name" | sed -e 's/\(.\)/\1\n/g' | grep -c "/")
+	count=$(echo "$count 1 +pq" | dc)
+	tmp=$(echo "$file_name" | replace "$(echo "$file_name" | cut -d/ -f$count)" "")
+	echo "!!!!!!!! $file_name $file_path $tmp !!!!!!!!!!!"
+	if ! [ -e "$tmp" ] && [ -n "$tmp" ]
+	then
+		mkdir -p "$tmp"
+	fi
+
+	if [ -e "$file_name" ] && [ -e "$file_path" ]
+	then
+		if ! _diff "$file_path" "$file_name"
+		then
+			echo "$CHANGE Updating $file_name! $END"
+			cp "$file_path" "$file_name"
+		else
+			echo "$UPTODATE $file_name is up-to-date! $END"
+		fi
+	elif ! [ -e "$file_name" ] && [ -e "$file_path" ]
+	then
+		echo "$CHANGE Creating $file_name! $END"
+		cp "$file_path" "$file_name"
+	elif [ ! -e "$file_path" ]
+	then
+		die "$file_path does not exist!"
+	fi
+}
+
 update()
 {
-	if [ -z "$2" ] && [ -e "$HOME/.$1" ]
+	if [ "$(file "$1" | cut -d ":" -f2)" = " directory" ]
 	then
-		if ! _diff "$1" "$HOME/.$1"
-		then
-			echo "$CHANGE Updating $1! $END"
-			cp "$HOME/.$1" "$1"
-		else
-			echo "$UPTODATE $1 is up-to-date! $END"
-		fi
-	elif ! [ -z "$2" ]
-	then
-		mkdir "$1"
-		echo "$CHANGE Updating $1... $END"
-		if [ -z "$3" ]
-		then
-			for file in $2
-			do
-				cp -r "$file" "$1"
-			done
-		else
-			for file in $2
-			do
-				case $file in
-					*$3*) ignore="y" ;;
-				esac
-				if [ -z "$ignore" ]
-				then
-					cp -r "$file" "$1"
-				else
-					echo "$IGNORE Ignoring $file! $END"
-				fi
-				ignore=""
-			done
-		fi
+		update_dir "$1" "$2"
 	else
-		echo "$ERROR Cannot update $1 $END"
+		update_file "$1" "$2"
 	fi
 }
 
 
-update program_repos
+update_dir "/home/zocki/.config/scripts" "unicode.sh"
+update_dir "/home/zocki/.config/env"
+# update /home/zocki/.program_repos
+# update_file /home/zocki/.program_repos "progs"
+update_file /home/zocki/.2048
