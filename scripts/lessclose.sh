7c8d9d6225e4140eb090c14115b121637aa6c0cd
zocker <zocker@10zen.eu>
2022-01-04 17:40:44 +0100

    Wrap lines in less

---
+++ scripts/lessclose.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+if [ -e /tmp/less.lock ]
+then
+	rm "/tmp/less" "/tmp/less.lock"
+fi
---
+++ scripts/lessopen.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+TMP="/tmp/less"
+fold -sw 80 < "$1" > "$TMP" && touch /tmp/less.lock
+echo "$TMP"
