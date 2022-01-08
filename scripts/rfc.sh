ca1fe160f926cba1323d849a360a8729efdbaa0a
zocker <zocker@10zen.eu>
2022-01-09 00:52:33 +0100

    Updates to rfc.sh
    This comes with an update to pad!
    
    With some changes the output of rfc.sh will now be centered!
    Wohoo!

--- scripts/rfc.sh
+++ scripts/rfc.sh
@@ -2,5 +2,9 @@
 if [ -z "$1" ]; then exit 1; fi
 TMP=$(mktemp)
 BASE="https://rfc-editor.org/rfc"
-curl -sL "$BASE/rfc$1.txt" | sed 's///' > "$TMP"
+curl -sL "$BASE/rfc$1.txt" | sed 's///' |  while read -r line
+do
+	pad -m "center" -s "$line" >> "$TMP.new"
+done
+mv "$TMP.new" "$TMP"
 less -sNJL "$TMP" && rm "$TMP"
