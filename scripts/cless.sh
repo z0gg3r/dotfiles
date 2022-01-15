0bb2973f4d7e3602d01e573b1d73e23606b9132e
zocker <zocker@10zen.eu>
2022-01-15 19:13:38 +0100

    Script to center less output

---
+++ scripts/cless.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+if [ -z "$1" ]
+then
+	echo "No arguments provided"
+	exit 1
+fi
+
+if ! [ -e "$1" ]
+then
+	echo "$1 does not exist"
+	exit 1
+fi
+
+cat "$1" | fold -s | while read -r line
+do
+	pad -m center -s "$line"
+done | less
