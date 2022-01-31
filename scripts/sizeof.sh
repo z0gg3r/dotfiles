b577e193ade4fe52fcb8a66da5337caf5d0189c3
zocker <zocker@10zen.eu>
2022-01-31 09:56:03 +0100

    Report sizeof files and dirs

---
+++ scripts/sizeof.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+die()
+{
+	echo "Could not enter directory $1"
+	exit 1
+}
+
+size_of_dir()
+{
+	cwd="$(pwd)"
+	cd "$1" || die "$1"
+	for dir in *
+	do
+		/usr/bin/du -sh "$dir"
+	done | sort -h
+	dir=$(du -sh "." | cut -d "$(printf "\t")" -f1)
+	printf "%b\t%b\n" "$dir" "$1"
+	cd "$cwd" || die "$cwd"
+}
+
+if [ -z "$1" ]
+then
+	exit 1
+fi
+echo "sizeof $1"
+
+if [ "$(file "$1" | cut -d ":" -f2)" = " directory" ]
+then
+	size_of_dir "$1"
+else
+	/usr/bin/du -sh "$1"
+fi
+
+shift
+while [ "$#" -ge 1 ]
+do
+	echo "sizeof $1"
+	if [ "$(file "$1" | cut -d ":" -f2)" = " directory" ]
+	then
+		size_of_dir "$1"
+	else
+		/usr/bin/du -sh "$1"
+	fi
+	shift
+done
