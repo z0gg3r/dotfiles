19084a8c91778524e53ee65f2714984c04186f2c
zocker <zocker@10zen.eu>
2022-05-12 09:12:21 +0200

    Changes

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
-touch "$HOME/.wallpaper_change"
+touch $HOME/.wallpaper_change
 date=$(date +%Y-%m-%d)
 time=$(date +%H:%M:%S)
 dir="$HOME/wallpapers"
 wallpaper="$dir/$1.png"
-ln -sf "$wallpaper" "$HOME/.wallpaper.png"
+ln -sf "$wallpaper" $HOME/.wallpaper.png
 num="$(echo "$1 + 1" | calc -p)"
-name="$(lecho -c -f "$HOME/.wallpapermap" -l "$num" | trim)"
-echo "[Manual Change]    $date @ $time -> $wallpaper ($name)" >> "$HOME/.config/wallpaperlog"
+name="$(lecho -c -f $HOME/.wallpapermap -l "$num" | trim)"
+echo "[Manual Change]    $date @ $time -> $wallpaper ($name)" >> $HOME/.config/wallpaperlog
--- scripts/unicode.sh
+++ scripts/unicode.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# chosen="$(cut -d ';' -f1 $HOME/.config/codemap | rofi -dmenu | sed "s/ .*//")"
+# chosen="$(cut -d ';' -f1 /$HOME/.config/codemap | rofi -dmenu | sed "s/ .*//")"
 chosen="$(cut -d ';' -f1 $HOME/.config/codemap | rofi -dmenu)"
 
 [ -z "$chosen" ] && exit
