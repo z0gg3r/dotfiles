3eb7aab58d1a8c231a74e942df3f0555a36c3556
zocker <zocker@10zen.eu>
2022-05-13 07:39:16 +0200

    Add a quick diff function
    Still needs some tuning. Maybe output both du -s and the formatted
    values. We'll see.

--- scripts/git-compress.sh
+++ scripts/git-compress.sh
@@ -7,15 +7,47 @@ die()
 	exit 1
 }
 
+size_format()
+{
+	if [ -z "$2" ]
+	then
+		num="$1"
+	else
+		num="$(echo "$1" | cut -d "$(printf "\t")" -f1)"
+		dir="$(echo "$1" | cut -d "$(printf "\t")" -f2)"
+	fi
+	unit="K"
+
+	num=$(echo "$num / 1024" | calc -p)
+
+	if [ -z "$dir" ]
+	then
+		printf "%.2f%b" "$num" "$unit"
+	else
+		printf "%.2f%b\t%b" "$num" "$unit" "$dir"
+	fi
+}
+
+size_diff()
+{
+	before="$(echo "$1" | cut -d "$(printf "\t")" -f1)"
+	after="$(echo "$2" | cut -d "$(printf "\t")" -f1)"
+	diff=$(echo "$after - $before" | calc -p)
+	diff=$(echo "$diff / 1024" | calc -p)
+	printf "%b\n" "$(size_format "$diff")"
+
+}
+
 git_compress()
 {
 	cd "$1" || die "cannot cd into $1"
-	before=$(du -sh ".git")
-	printf "before:\t%b\n" "$before" | replace ".git" "$1"
+	before=$(du -s ".git")
+	printf "before:\t%b\n" "$(size_format "$before" ".")" | replace ".git" "$1"
 	git repack -AdflF 2> /dev/null
-	after=$(du -sh ".git")
+	after=$(du -s ".git")
 	cd "$2" || die "cannot cd into $2"
-	printf "after:\t%b\n" "$after" | replace ".git" "$1"
+	printf "after:\t%b\n" "$(size_format "$after" ".")" | replace ".git" "$1"
+	printf "diff:\t%b\n" "$(size_diff "$before" "$after")"
 }
 
 if [ -z "$TARGET" ]
@@ -31,8 +63,8 @@ else
 		TARGET="$cwd/$TARGET"
 	fi
 	cd "$TARGET" || die "cannot cd into $TARGET"
-	before=$(du -sh "$TARGET")
-	printf "before:\t%b\n\n" "$before"
+	before=$(du -s "$TARGET")
+	printf "before:\t%b\n\n" "$(size_format "$before" ".")"
 	for dir in *
 	do
 		if [ -d "$dir" ] && [ -e "$dir/.git" ]
@@ -40,8 +72,9 @@ else
 			git_compress "$dir" "$TARGET"
 		fi
 	done
-	after=$(du -sh "$TARGET")
-	printf "\nafter:\t%b\n" "$after"
+	after=$(du -s "$TARGET")
+	printf "\nafter:\t%b\n" "$(size_format "$after" ".")"
+	printf "diff:\t%b\n" "$(size_diff "$before" "$after")"
 	cd "$cwd" || die "cannot cd into $cwd"
 fi
 
