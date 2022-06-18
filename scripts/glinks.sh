4dfea978d7771b1fe94abe0eedd9e47e811c7990
zocker <zocker@10zen.eu>
2022-06-18 14:36:41 +0200

    Firejail that shit

--- scripts/glinks.sh
+++ scripts/glinks.sh
@@ -1,6 +1,16 @@
 #! /bin/sh
 
-links -g \
+firejail \
+	--noprofile \
+	--hosts-file="$HOME/.config/adblocklist" \
+	--private="$HOME/Documents/mail_viewer" \
+	--nodvd \
+	--nosound \
+	--notv \
+	--nou2f \
+	--novideo \
+	--private-cache \
+	links -g \
 	-force-html \
 	-no-connect \
 	-save-cookies 0 \
