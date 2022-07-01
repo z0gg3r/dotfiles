5cdb964804b1ab2373a23b08d8f926ba1901b07b
zocker <zocker@10zen.eu>
2022-07-01 18:41:40 +0200

    Script to sum csv-files

---
+++ scripts/sum.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+if ! [ -e "$1" ]
+then
+	echo "$1 does not exist"
+fi
+
+if [ -z "$2" ]
+then
+	echo "Cannot determine which field to sum."
+fi
+
+CSV="$1"
+FIELD="$2"
+DELIM="$3"
+
+if [ -z "$DELIM" ]
+then
+	DELIM=","
+fi
+
+cut -d "$DELIM" -f "$FIELD" "$CSV" | paste -s -d + | calc -p
