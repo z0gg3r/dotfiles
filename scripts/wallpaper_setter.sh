2bf04935366077007afb9c4bd2e94dcc7c5c324a
zocker <zocker@10zen.eu>
2022-07-05 21:41:56 +0200

    Add option for random wallpaper
    Yaay

--- scripts/wallpaper_setter.sh
+++ scripts/wallpaper_setter.sh
@@ -1,11 +1,17 @@
 #! /bin/sh
 
-choice="$(rofi -dmenu < "$HOME/.wallpapermap" | cut -d, -f1)"
+list="$(printf "%b\nrandom" "$(cat "$HOME"/.wallpapermap)")"
+choice="$(echo "$list" | rofi -dmenu | cut -d, -f1)"
 
 if [ -z "$choice" ]
 then
 	exit 1
 fi
 
-change_wallpaper.sh "$choice"
+if [ "$choice" = "random" ]
+then
+	wallpaper.sh
+else
+	change_wallpaper.sh "$choice"
+fi
 feh --bg-fill "$HOME/.wallpaper.png"
