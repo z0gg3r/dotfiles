e578dd965c21f693bde446e1571181888b53088e
zocker <zocker@10zen.eu>
2022-07-05 21:41:30 +0200

    Don't do that...

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -5,7 +5,7 @@ pgrep uim-toolbar > /dev/null || daemonize "$(which uim-toolbar-gtk3-systray)"
 pgrep flameshot > /dev/null || daemonize "$(which flameshot)"
 pgrep caffeine > /dev/null || daemonize "$(which caffeine)"
 pgrep dunst > /dev/null || daemonize "$(which dunst)"
-pgrep pipewire > /dev/null || exec gentoo-pipewire-launcher
+pgrep pipewire > /dev/null || daemonize "$(which gentoo-pipewire-launcher)"
 
 sct 6000 0.85
 numlockx on
