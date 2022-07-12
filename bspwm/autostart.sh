35d213b0d7d43aab82a27eb4ceec5b999b8205cf
zocker <zocker@10zen.eu>
2022-07-12 12:05:44 +0200

    Remove gnome polkit

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-pgrep /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 > /dev/null || daemonize /usr/lib/polkit-gnome/polkti-gnome-authentication-agent-1
 pgrep uim-toolbar > /dev/null || daemonize "$(which uim-toolbar-gtk3-systray)"
 pgrep flameshot > /dev/null || daemonize "$(which flameshot)"
 pgrep caffeine > /dev/null || daemonize "$(which caffeine)"
