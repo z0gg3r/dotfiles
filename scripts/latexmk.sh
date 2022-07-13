4faff9e16288ed275eff2d064fba3649940087df
zocker <zocker@10zen.eu>
2022-07-13 16:41:16 +0200

    Expansion for this :)

--- scripts/latexmk.sh
+++ scripts/latexmk.sh
@@ -1,4 +1,25 @@
 #! /bin/sh
+
+build_all()
+{
+	for file in *
+	do
+		ltx_build "$file"
+	done
+}
+
+ltx_build()
+{
+	latexmk -xelatex "$1"
+	latexmk -c
+}
+
+if [ -z "$1" ]
+then
+	build_all
+	exit
+fi
+
 if ! [ -e "$1" ]
 then
 	echo "$1 does not exist"
