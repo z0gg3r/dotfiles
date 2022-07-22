a5254d59b141d87336a5a6c4d1d1fe44f8042a17
zocker <zocker@10zen.eu>
2022-07-22 10:50:28 +0200

    Script to time executions

---
+++ scripts/timer.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+time_stamp()
+{
+	file="/tmp/$1"
+	if [ -e "$file" ]
+	then
+		start=$(cat "$file")
+		now=$(/bin/date "+%s")
+		diff=$(echo "$now - $start" | calc -p)
+		printf "Took %bs\n" "$diff"
+		rm "$file"
+	else
+		/bin/date "+%s" > "$file"
+	fi
+}
+
+if [ -z "$1" ]
+then
+	exit 1
+fi
+
+case $1 in
+	"anonymous")
+		time_stamp "anontimestamp" ;;
+	"start")
+		id=$(uuidgen)
+		time_stamp "$id"
+		echo "$id"
+		;;
+	*)
+		time_stamp "$1" ;;
+esac
