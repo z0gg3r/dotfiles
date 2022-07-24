a37544a54041471a4a43c58cf0db4e9261f4b7f4
zocker <zocker@10zen.eu>
2022-07-24 09:02:26 +0200

    Works like this again

--- scripts/wacom.sh
+++ scripts/wacom.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-xinput set-prop "Wacom Intuos Pro S Pen Pen (0x878038e9)" --type=float "Coordinate Transformation Matrix" 0.333333 0 0.333333 0 1 0 0 0 1
-#xinput set-prop "Wacom Intuos Pro S Pen eraser" --type=float "Coordinate Transformation Matrix" 0.333333 0 0.333333 0 1 0 0 0 1
-# xsetwacom set "Wacom Intuos Pro S Pad pad" Button 2 "key +ctrl z -ctrl"
-# xsetwacom set "Wacom Intuos Pro S Pad pad" Button 3 "key +ctrl +alt l -alt -ctrl"
+xinput set-prop "Wacom Intuos Pro S Pen stylus" --type=float "Coordinate Transformation Matrix" 0.333333 0 0.333333 0 1 0 0 0 1
+xinput set-prop "Wacom Intuos Pro S Pen eraser" --type=float "Coordinate Transformation Matrix" 0.333333 0 0.333333 0 1 0 0 0 1
+xsetwacom set "Wacom Intuos Pro S Pad pad" Button 2 "key +ctrl z -ctrl"
+xsetwacom set "Wacom Intuos Pro S Pad pad" Button 3 "key +ctrl +alt l -alt -ctrl"
