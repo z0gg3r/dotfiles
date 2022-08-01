631e09c9b8e92729df594e745b18d32e51bc1979
zocker <zocker@10zen.eu>
2022-08-01 19:02:35 +0200

    Add more dir's

--- scripts/bincount.sh
+++ scripts/bincount.sh
@@ -1,11 +1,19 @@
 #! /bin/sh
-bin="$(/bin/ls /usr/bin | wc -l)"
+usr_bin="$(/bin/ls /usr/bin | wc -l)"
+bin="$(/bin/ls /bin | wc -l)"
 local_bin="$(/bin/ls /usr/local/bin | wc -l)"
+home_bin="$(/bin/ls /home/zocki/.local/bin | wc -l)"
+sbin="$(/bin/ls /sbin | wc -l)"
+usr_sbin="$(/bin/ls /usr/sbin | wc -l)"
 
-dc_args="$bin $local_bin + pq"
+dc_args="$bin $usr_bin $local_bin $home_bin $sbin $usr_sbin +++++ pq"
 
 total="$(echo "$dc_args" | dc)"
 
-echo "/usr/bin: $bin"
+echo "/bin: $bin"
+echo "/usr/bin: $usr_bin"
 echo "/usr/local/bin: $local_bin"
+echo "$HOME/.local/bin: $home_bin"
+echo "/sbin: $sbin"
+echo "/usr/sbin: $usr_sbin"
 echo "total: $total"
