71fe84425845348d9f99ef03b6153a49e721205f
zocker <zocker@10zen.eu>
2023-01-06 16:35:30 +0100

    Start Polkit-Auth-Agent

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -5,6 +5,7 @@ pgrep flameshot > /dev/null || daemonize "$(which flameshot)"
 pgrep caffeine > /dev/null || daemonize "$(which caffeine)"
 pgrep dunst > /dev/null || daemonize "$(which dunst)"
 pgrep pipewire > /dev/null || daemonize "$(which gentoo-pipewire-launcher)"
+pgrep polkit-gnome-authentication-agent-1 > /dev/null || daemonize "/usr/libexec/polkit-gnome-authentication-agent-1"
 
 sct 6000 0.7
 numlockx on
