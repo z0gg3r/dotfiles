c6d0eafe784de5b189a71dcd2e028d23b0276383
zocker <zocker@10zen.eu>
2023-04-09 23:54:38 +0200

    Add trurl version

--- scripts/gitignore.sh
+++ scripts/gitignore.sh
@@ -1,10 +1,36 @@
 #!/bin/sh
-gi="https://gitignore.io/api/$1"
+
+[ -z "$1" ] && exit 1
+
+legacy()
+{
+	gi="https://gitignore.io/api/$1"
+	shift
+	while [ $# -ge 1 ]
+	do
+		gi="$gi,$1"
+		shift
+	done
+
+	curl -sL "$gi"
+}
+
+if ! command -V trurl > /dev/null
+then
+	legacy "$@"
+	exit
+fi
+
+BASE_URI="https://gitignore.io/api"
+request=""
+
+request=$(trurl "$BASE_URI" --query-separator , -a "query=$1")
 
 shift
 while [ $# -ge 1 ]
 do
-	gi="$gi,$1"
+	request=$(trurl "$request" --query-separator , -a "query=$1")
 	shift
 done
-curl -sL "$gi"
+
+echo "$request" | tr '?' '/' | xargs curl -sL
