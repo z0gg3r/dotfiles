861dce7bd4e46fa99989f9fa80f0cc4397490db1
zocker <zocker@10zen.eu>
2023-05-17 18:52:59 +0200

    scripts: remove test-wayland sections

--- scripts/emoji.sh
+++ scripts/emoji.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-
-if test-wayland.sh
-then
-	chosen="$(cut -d ';' -f1 "$HOME/.config/emojimap" | wofi -d | sed "s/ .*//")"
-	[ -z "$chosen" ] && exit
-	wtype "$chosen"
-else
-	chosen="$(cut -d ';' -f1 "$HOME/.config/emojimap" | rofi -dmenu | sed "s/ .*//")"
-	[ -z "$chosen" ] && exit
-	xdotool type "$chosen"
-fi
+chosen="$(cut -d ';' -f1 "$HOME/.config/emojimap" | rofi -dmenu | sed "s/ .*//")"
+[ -z "$chosen" ] && exit
+xdotool type "$chosen"
--- scripts/unicode.sh
+++ scripts/unicode.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if test-wayland.sh
-then
-	chosen="$(wofi -d < "$HOME/.config/codemap" | cut -d ';' -f1)"
-	[ -z "$chosen" ] && exit
-	wtype "$chosen"
-else
-	chosen="$(rofi -normalize-match -matching fuzzy -dmenu < "$HOME/.config/codemap" | cut -d ';' -f1)"
-	[ -z "$chosen" ] && exit
-	xdotool type "$chosen"
-fi
+chosen="$(rofi -normalize-match -matching fuzzy -dmenu < "$HOME/.config/codemap" | cut -d ';' -f1)"
+[ -z "$chosen" ] && exit
+xdotool type "$chosen"
