fc812731bdeefaacb7bac24b9a8da7402263e354
zocker <zocker@10zen.eu>
2023-07-04 20:17:36 +0200

    scirpts: switch-xbkmap: A script to switch keyboard layouts
    Currently toggles between us and jp layout. Could be changed for more
    toggles or different toggles. Also can be used as a setxkbmap-wrapper if
    an arguemnt is passed, because why not. :)

---
+++ scripts/switch-xkbmap.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+change_xkb_map()
+{
+	setxkbmap "$1"
+}
+
+get_xkb_map()
+{
+	setxkbmap -print -verbose 10 | grep layout | cut -d: -f2 | trim
+}
+
+toggle_xkb_map()
+{
+	map="$(get_xkb_map)"
+	if [ "$map" = "us" ]
+	then
+		change_xkb_map jp
+	else
+		change_xkb_map us
+	fi
+}
+
+if [ -z "$1" ]
+then
+	toggle_xkb_map
+else
+	switch_xkb_map "$1"
+fi
