88380a3d9533267801ce2632fff74fe38841eb6e
zocker <zocker@10zen.eu>
2023-07-14 17:07:46 +0200

    scripts: switch-xkbmap: Change how this works

--- scripts/switch-xkbmap.sh
+++ scripts/switch-xkbmap.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
-change_xkb_map()
-{
-	setxkbmap "$1"
-}
-
 get_xkb_map()
 {
-	setxkbmap -print -verbose 10 | grep layout | cut -d: -f2 | trim
+	setxkbmap -print -verbose 10 | grep variant | cut -d: -f2 | trim
 }
 
 toggle_xkb_map()
 {
 	map="$(get_xkb_map)"
+
 	if [ "$map" = "us" ]
 	then
-		change_xkb_map ja us
+		setxkbmap ja kana
 	else
-		change_xkb_map us
+		setxkbmap ja us
 	fi
 }
 
@@ -25,5 +21,5 @@ if [ -z "$1" ]
 then
 	toggle_xkb_map
 else
-	switch_xkb_map "$1"
+	setxkbmap "$1"
 fi
