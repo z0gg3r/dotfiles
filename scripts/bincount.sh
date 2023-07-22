464f31f953327e409a7d23e831cd0b45d097e3a1
zocker <zocker@10zen.eu>
2023-07-22 11:48:16 +0200

    scripts: bincount: count scripts as well
    also create a proper counting function :P

--- scripts/bincount.sh
+++ scripts/bincount.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-usr_bin="$(/bin/ls /usr/bin | wc -l)"
-bin="$(/bin/ls /bin | wc -l)"
-local_bin="$(/bin/ls /usr/local/bin | wc -l)"
-home_bin="$(/bin/ls /home/zocki/.local/bin | wc -l)"
-sbin="$(/bin/ls /sbin | wc -l)"
-usr_sbin="$(/bin/ls /usr/sbin | wc -l)"
 
-dc_args="$bin $usr_bin $local_bin $home_bin $sbin $usr_sbin +++++ pq"
+get_bin_cnt()
+{
+	[ -e "$1" ] && find "$1" | wc -l
+}
+
+bin="$(get_bin_cnt "/bin")"
+sbin="$(get_bin_cnt "/sbin")"
+usr_bin="$(get_bin_cnt "/usr/bin")"
+usr_sbin="$(get_bin_cnt "/usr/sbin")"
+local_bin="$(get_bin_cnt "/usr/local/bin")"
+home_bin="$(get_bin_cnt "$HOME/.local/bin")"
+script_bin="$(get_bin_cnt "$HOME/.local/scripts")"
+
+dc_args="$bin $sbin $usr_bin $usr_sbin $local_bin $home_bin $script_bin ++++++ pq"
 
 total="$(echo "$dc_args" | dc)"
 
@@ -14,6 +21,7 @@ echo "/bin: $bin"
 echo "/usr/bin: $usr_bin"
 echo "/usr/local/bin: $local_bin"
 echo "$HOME/.local/bin: $home_bin"
+echo "$HOME/.local/scripts: $script_bin"
 echo "/sbin: $sbin"
 echo "/usr/sbin: $usr_sbin"
 echo "total: $total"
