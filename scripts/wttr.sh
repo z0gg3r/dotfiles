617fcc2d1a8c96f0088f789a59d7d04e19921254
zocker <zocker@10zen.eu>
2023-10-03 17:06:07 +0200

    scripts: wttr: Also name $2
    $2 is the country.

--- scripts/wttr.sh
+++ scripts/wttr.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 WTTR="https://wttr.in"
 CITY="$1"
+COUNTRY="$2"
 
 if [ -z "$CITY" ]
 then
 	exit 1
 fi
 
-if [ -z "$2" ]
+if [ -z "$COUNTRY" ]
 then
 	curl -sL "$WTTR/$CITY"
 else
-	curl -sL "$WTTR/$CITY,$2"
+	curl -sL "$WTTR/$CITY,$COUNTRY"
 fi
