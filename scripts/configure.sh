1ee5c315fb68bfaa31e24705ba305795f29e3440
zocker <zocker@10zen.eu>
2023-10-21 11:06:30 +0200

    scripts/configure: Change how this works
    ;)

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,8 +1,55 @@
 #!/bin/sh
+#shellcheck disable=2086
+#shellcheck disable=2153
 
-pwd=$(pwd)
+CONFIGURE_REGISTER="$HOME/.config/configures"
+CONFIGURE_CONFIGS="$HOME/.local/share/configure.sh"
 
-chosen=$(find "$HOME/.config/configures" | fzy)
+get_opts()
+{
+	cat "$CONFIGURE_CONFIGS/$1/opts"
+}
+
+get_cflags()
+{
+	cat "$CONFIGURE_CONFIGS/$1/cflags" 2> /dev/null || printf '%b' "$CFLAGS"
+}
+
+get_ldflags()
+{
+	cat "$CONFIGURE_CONFIGS/$1/ldflags" 2> /dev/null || printf '%b' "$LDFLAGS"
+}
+
+get_cxxflags()
+{
+	cat "$CONFIGURE_CONFIGS/$1/cxxflags" 2> /dev/null || printf '%b' "$CXXFLAGS"
+}
+
+do_configure()
+{
+	opts="$(get_opts "$1")"
+	cflags="$(get_cflags "$1")"
+	ldflags="$(get_ldflags "$1")"
+	cxxflags="$(get_cxxflags "$1")"
+
+	configure "$opts" "$cflags" "$ldflags" "$cxxflags"
+}
+
+configure()
+{
+	if [ -e './configure' ]
+	then
+		CFLAGS="$2" LDFLAGS="$3" CXXFLAGS="$4" ./configure $1
+		printf "Configure options were: %b\n" "$1"
+		printf "CFLAGS: %b\n" "$2"
+		printf "LDFLAGS: %b\n" "$3"
+		printf "CXXFLAGS: %b\n" "$4"
+	else
+		printf 'CFLAGS="%b" LDFLAGS="%b" CXXFLAGS="%b" ./configure %b\n' "$2" "$3" "$4" "$1"
+	fi
+}
+
+chosen=$(find "$CONFIGURE_REGISTER" -type f | fzy | xargs basename)
 
 if [ -z "$chosen" ]
 then
@@ -10,16 +57,4 @@ then
 	exit
 fi
 
-#config=$(cat "$HOME/.config/configures/$chosen")
-config="$(cat "$chosen")"
-
-cd "$HOME/programs/$(basename "$chosen")" || exit
-
-./configure $config
-
-echo "Configure options were: $config"
-echo "CFLAGS: $CFLAGS"
-echo "LDFLAGS: $LDFLAGS"
-echo "CXXFLAGS: $CXXFLAGS"
-
-cd "$pwd" || exit
+do_configure "$chosen"
