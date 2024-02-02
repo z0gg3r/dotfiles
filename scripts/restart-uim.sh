1597a54bf1bd4e7044db6ff17b8edd3d487de88c
zocker <zocker@10zen.eu>
2024-02-02 20:30:53 +0100

    scripts: restart-uim: Drop this
    I've used ibus for quite some time now and am not looking to switch bach
    to uim :)

--- scripts/restart-uim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-killall uim-helper-server
-killall uim-candwin-gtk3
-killall uim-toolbar-gtk3-systray
-killall uim-xim
-
-daemonize $(which uim-xim)
-daemonize $(which uim-toolbar-gtk3-systray)
