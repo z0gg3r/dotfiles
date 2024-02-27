c9bbe7164b0e7319fee8ad44db104c4981b175a9
zocker <zocker@10zen.eu>
2024-02-27 16:52:03 +0100

    scripts: unicode: Also add entry to clipboard
    Works more reliably than xdotool type for non-terminal windows...

--- scripts/unicode.sh
+++ scripts/unicode.sh
@@ -3,3 +3,4 @@
 chosen="$(rofi -normalize-match -matching fuzzy -dmenu < "$HOME/.config/codemap" | cut -d ';' -f1)"
 [ -z "$chosen" ] && exit
 xdotool type "$chosen"
+printf '%b' "$chosen" | xclip -selection clipboard
