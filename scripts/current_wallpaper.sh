436c4c4ae27440608581ac113969145fbade19ac
zocker <zocker@10zen.eu>
2024-02-27 16:51:09 +0100

    scripts: current_wallpaper: Change wallpaper reporting
    I've added some more needles hours into my wallpaper setup so now it's
    slightly different.

--- scripts/current_wallpaper.sh
+++ scripts/current_wallpaper.sh
@@ -3,7 +3,8 @@
 . "$HOME/.local/scripts/wallpaper-exports.sh"
 
 wallpaper="$(readlink "$WALLPAPERS_DST" | cut -b 24- | replace ".png" "")"
+wallpaper_id="$(readlink "$WALLPAPERS_SRC/$wallpaper.png" | xargs basename)"
 line="$(echo "$wallpaper" + 1 | calc -p)"
-description="$(lecho -f "$WALLPAPERS_MAP" -l "$line" | cut -d, -f2 | trim | replace " " "_")"
+description="$(lecho -f "$WALLPAPERS_MAP" -l "$line" | cut -d, -f2 | trim)"
 
-echo "Current wallpaper is: $wallpaper.png or $description.png"
+printf 'Current wallpaper is %b (currently index %b), depicting %b%b%b\n' "$wallpaper_id" "$wallpaper" "'" "$description" "'"
