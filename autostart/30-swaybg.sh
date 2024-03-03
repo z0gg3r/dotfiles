9ad09192b38720941d7b807fa78562d4825addb7
zocker <zocker@10zen.eu>
2024-03-03 10:15:25 +0100

    autostart: Remove unused entries

--- autostart/30-swaybg.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-. "$HOME/.config/autostart/00-exports.sh"
-
-start3 swaybg -i "$HOME/.wallpaper.png" -m fill
--- autostart/30-waybar.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-. "$HOME/.config/autostart/00-exports.sh"
-
-pgrep waybar > /dev/null || start waybar
