c4d60d338a3548cf6565638484c5c3f088dd26c5
zocker <zocker@10zen.eu>
2024-06-19 20:11:07 +0200

    scripts: programs: Print update stat at the end :)

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -17,24 +17,31 @@ git_pull()
 {
 	if [ -e .git ]
 	then
+		cur="$(git log | head -n1 | cut -d' ' -f1)"
 		git remote get-url origin
 		GIT_CONFIG_GLOBAL="/dev/null" git pull --no-rebase --ff --recurse-submodules
+		new="$(git log | head -n1 | cut -d' ' -f1)"
+
+		[ "$cur" != "$new" ] && printf '%b\t->\t%b..%b\n' "$(pwd)" "$cur" "$new" | tee -a /tmp/update
 	else
 		printf "%b%b is not a git repo.%b\n" "$ERROR" "$(pwd)" "$END"
 	fi
 }
 
-curr_dir=$(pwd)
+rm -f /tmp/update
+cwd=$(pwd)
 cd "$PROG_DIR" || die
 
 for dir in *
 do
 	(
-	cd "$dir" || die
-	git_pull
-	printf "%b%b%b\n" "$BLUE" "$BANNER" "$END"
-	printf "%b%b%b\n" "$YELLOW" "$BANNER" "$END"
-)
+		cd "$dir" || die
+		git_pull
+		printf "%b%b%b\n" "$BLUE" "$BANNER" "$END"
+		printf "%b%b%b\n" "$YELLOW" "$BANNER" "$END"
+	)
 done
 
-cd "$curr_dir" || die
+[ -e /tmp/update ] && printf 'The following programs have been updated:\n' && cat /tmp/update
+rm -f /tmp/update
+cd "$cwd" || die
