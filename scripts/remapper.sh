1a3b2922e6b32d73c010eb61342fb8de49356711
zocker <zocker@10zen.eu>
2024-11-15 15:13:24 +0100

    scripts: remapper: Add DOTOOL_KEY

--- scripts/remapper.sh
+++ scripts/remapper.sh
@@ -3,10 +3,12 @@
 if [ "$XDG_SESSION_TYPE" = "wayland" ]
 then
 	export DOTOOL="wtype -d 15"
+	export DOTOOL_KEY="wtype -k"
 	export CLIPTOOL="wl-clopy"
 	export BGTOOL="swaybg -m fill -i"
 else
 	export DOTOOL="xdotool type --delay 15"
+	export DOTOOL_KEY="xdotool key"
 	export CLIPTOOL="xclip -selection clipboard"
 	export BGTOOL="feh --no-fehbg --bg-fill"
 fi
