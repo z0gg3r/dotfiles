ee5830183488c20e4860c2d637a11e77c34d5b07
zocker <zocker@10zen.eu>
2024-11-21 17:45:29 +0100

    scripts: muttuseragent: RIP Bram

--- scripts/muttuseragent.sh
+++ scripts/muttuseragent.sh
@@ -15,4 +15,4 @@ get_msmtp_version()
 	msmtp --version | sed 1q | sed 's/ version//g'
 }
 
-printf '%b; %b; %b\n' "$(get_neomutt_version)" "$(get_vim_version)" "$(get_msmtp_version)"
+printf '%b; %b (RIP Bram); %b\n' "$(get_neomutt_version)" "$(get_vim_version)" "$(get_msmtp_version)"
