0da61f6da59b319172e4142d438807fd455757cf
zocker <zocker@10zen.eu>
2025-02-14 14:52:28 +0100

    scripts: add_bookmark: Default to bookmarks.db

--- scripts/add_bookmark.sh
+++ scripts/add_bookmark.sh
@@ -2,7 +2,7 @@
 
 title="$1"
 url="$2"
-db="$3"
+db="$HOME/.config/pmb/bookmarks.db"
 
 if [ -z "$title" ]
 then
@@ -14,19 +14,6 @@ if [ -z "$url" ]
 then
 	printf "Enter url: "
 	read -r url
-fi
-
-if [ -z "$db" ]
-then
-	printf "Enter database: "
-	read -r db
-fi
-
-if [ -z "$db" ]
-then
-	db="$HOME/.config/pmb/bookmarks.db"
-else
-	db="$HOME/.config/pmb/$db.db"
 fi
 
 if [ -z "$title" ] || [ -z "$url" ]
