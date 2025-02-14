94643455d24763a20a8a3e737cd1fdb669ef5b48
zocker <zocker@10zen.eu>
2025-02-14 14:58:13 +0100

    polybar: launch: switch to ${HOME}

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,6 @@
 killall -q polybar
 
 
-MONITOR="HDMI-0" polybar -c /home/zocki/.config/polybar/config main &
-MONITOR="HDMI-1" polybar -c /home/zocki/.config/polybar/config side &
-MONITOR="DVI-D-0" polybar -c /home/zocki/.config/polybar/config side &
+MONITOR="HDMI-0" polybar -c "${HOME}/.config/polybar/config" main &
+MONITOR="HDMI-1" polybar -c "${HOME}/.config/polybar/config" side &
+MONITOR="DVI-D-0" polybar -c "${HOME}/.config/polybar/config" side &
