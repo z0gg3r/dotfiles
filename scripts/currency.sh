01a2b483f9d02d76a6e61e4c36c63320ff0ddb92
zocker <zocker@10zen.eu>
2025-02-14 14:52:47 +0100

    scripts: currency: Finish the work on this one

--- scripts/currency.sh
+++ scripts/currency.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 # shellcheck disable=SC2140
-echo "grep "formatted_amounts=\[" | replace "formatted_amounts=[" "" | replace "]" "" | replace "\"" "" | cut -d, -f2"
+
+die()
+{
+	printf '%b\n' "$@"
+	exit 1
+}
+
+amount="$1"
+currency="$2"
+
+[ -z "$amount" ] && die "Must set amount"
+[ -z "$currency" ] && die "Must set currency"
+
+uri="https://currency.world/${amount}_${currency}"
+
+curl -s "${uri}" | grep 'formatted_amounts=\[' | replace 'formatted_amounts=['  '' | replace ']' ''  | replace '"' ''  | cut -d, -f2
