4ef039e631ba01ed3ebaedae8f5c53eaa30e3c57
zocker <zocker@10zen.eu>
2025-06-25 21:47:17 +0200

    scripts: use-grep: Prefer ag if present

--- scripts/use-grep.sh
+++ scripts/use-grep.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-grep "$1" /etc/portage/package.use/*
+if ! command -V ag > /dev/null 2> /dev/null
+then
+	grep "${1}" /etc/portage/package.use/*
+else
+	ag "${1}" /etc/portage/package.use
+fi
