fbba987352e7b5a3ef3046eb1fbc7be6f6deee66
zocker <zocker@10zen.eu>
2025-12-25 14:22:45 +0100

    scripts: compile-kernel: Add die() function

--- scripts/compile-kernel.sh
+++ scripts/compile-kernel.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
 
-cd /usr/src/linux || exit 1
+die()
+{
+	printf '%b\n' "${@}"
+	__exit
+}
+
+__exit()
+{
+	timer.sh kernel-compile
+	exit 1
+}
+
+trap __exit INT QUIT TERM
+
+timer.sh kernel-compile
+
+cd /usr/src/linux || die '/usr/src/linux does not exist'
 
 if ! [ -e '.config' ]
 then
 	zcat /proc/config.gz > .config
 
-	make olddefconfig || exit 1
+	make olddefconfig || die 'config provisioning failed'
 fi
 
-make -j24 -l30 || exit 1
+make -j24 -l30 || die 'kernel compile failed'
 
-make -j24 -l30 headers || exit 1
+make -j24 -l30 headers || die 'kernel header preparation failed'
 
-doas make modules_install headers_install install -j24 -l30 || exit 1
+doas make modules_install headers_install install -j24 -l30 || die 'kernel install failed'
 
-doas emerge -atv @module-rebuild || exit 1
+doas emerge -atv @module-rebuild || die 'module rebuild failed'
 
-doas dracut --reproducible --lz4 --early-microcode --printsize --force --kver "$(make kernelrelease)" || exit 1
+doas dracut --reproducible --lz4 --early-microcode --printsize --force --kver "$(make kernelrelease)" || die 'dracut failed'
 
-doas grub-mkconfig -o /boot/grub/grub.cfg || exit 1
+doas grub-mkconfig -o /boot/grub/grub.cfg || die 'update-grub failed'
+
+timer.sh kernel-compile
