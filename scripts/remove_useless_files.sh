793120b3ac7f1d9972b20914acbb7449e7036d49
zocker <zocker@10zen.eu>
2025-12-31 18:36:01 +0100

    scripts: remove_useless_files: quote ${HOME}
    Signed-off-by: zocker <zocker@10zen.eu>

--- scripts/remove_useless_files.sh
+++ scripts/remove_useless_files.sh
@@ -2,15 +2,15 @@
 
 remove_file()
 {
-	if [ -e "$1" ]
+	if [ -e "${1}" ]
 	then
-		rm "$1"
+		rm "${1}"
 	fi
 }
 
-remove_file $HOME/.calc_history
-remove_file $HOME/.wget-hsts
-remove_file $HOME/.xsession-errors.old
-remove_file $HOME/.sqlite_history
+remove_file "${HOME}/.calc_history"
+remove_file "${HOME}/.wget-hsts"
+remove_file "${HOME}/.xsession-errors.old"
+remove_file "${HOME}/.sqlite_history"
 
 notify-send "Removed useless files"
