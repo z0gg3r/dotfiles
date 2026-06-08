ce9251a1c02e569df1fce3be97e9e65a887c9804
zocker <zocker@10zen.eu>
2026-06-08 17:34:21 +0200

    scripts: compile-kernel updates
    Signed-off-by: zocker <zocker@10zen.eu>

--- scripts/compile-kernel.sh
+++ scripts/compile-kernel.sh
@@ -12,6 +12,15 @@ __exit()
 	exit 1
 }
 
+notify()
+{
+	notify-send \
+		--app-name='Kernel Compilation' \
+		--urgency=low \
+		--expire-time=2000 \
+		"${@}"
+}
+
 trap __exit INT QUIT TERM
 
 timer.sh kernel-compile
@@ -23,18 +32,25 @@ then
 	zcat /proc/config.gz > .config
 
 	make olddefconfig || die 'config provisioning failed'
+	notify 'Kernel configuration finished'
 fi
 
-make -j24 -l30 || die 'kernel compile failed'
+make -j24 -l30 V=12 || die 'kernel compile failed'
+notify 'Kernel compilation finished'
 
-make -j24 -l30 headers || die 'kernel header preparation failed'
+make -j24 -l30 headers V=12 || die 'kernel header preparation failed'
+notify 'Kernel header preperation finished'
 
-doas make modules_install headers_install install -j24 -l30 || die 'kernel install failed'
+INSTALL_HDR_PATH=/usr doas make modules_install headers_install install -j24 -l30 || die 'kernel install failed'
+notify 'Kernel installation finished'
 
 doas emerge -atv @module-rebuild || die 'module rebuild failed'
+notify 'Kernel module rebuild finished'
 
 doas dracut --reproducible --lz4 --early-microcode --printsize --force --kver "$(make kernelrelease)" || die 'dracut failed'
+notify 'Initramfs generation finished'
 
 doas grub-mkconfig -o /boot/grub/grub.cfg || die 'update-grub failed'
+notify 'Grub config update finished'
 
 timer.sh kernel-compile
